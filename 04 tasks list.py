4c58a9a23db198ce520a452465b08eb73b38915d
Nurbek2405 <nurbekorazov@gmail.com>
2024-12-08 22:19:01 +0500

    Разбил задание по темам

---
+++ 04 tasks list.py
@@ -0,0 +1,16 @@
+fruits = ["яблоко", "банан", "апельсин"]
+
+print(fruits)
+for fruit in fruits:
+    print(f"Я люблю {fruit}")
+
+'''list_fruits = ['lemons', 'bananas', 'oranges', 'apples', 'mangoes', 'kiwis' ]
+for i in range(len(list_fruits)):
+    print(list_fruits[i])'''
+
+# Задание решение
+'''friends = ['Dias','Rinat','Dulat','Igor','Bakutzhan']
+
+print(friends)
+for index, value in enumerate(friends):
+    print(f"Друг: {index+1}, Привет! {value}")'''
